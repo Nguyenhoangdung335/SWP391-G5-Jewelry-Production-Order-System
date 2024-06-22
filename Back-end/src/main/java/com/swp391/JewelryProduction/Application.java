df9b3c829f603abf1548dd147df66ca5feffc26b
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-06-22 10:37:53 +0700

    Update Application.java

--- Back-end/src/main/java/com/swp391/JewelryProduction/Application.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/Application.java
@@ -1,38 +1,33 @@
 package com.swp391.JewelryProduction;
 
 import com.github.javafaker.Faker;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import com.swp391.JewelryProduction.enums.AccountStatus;
+import com.swp391.JewelryProduction.enums.Gender;
 import com.swp391.JewelryProduction.enums.Role;
 import com.swp391.JewelryProduction.pojos.Account;
+import com.swp391.JewelryProduction.pojos.UserInfo;
 import com.swp391.JewelryProduction.repositories.AccountRepository;
-import com.swp391.JewelryProduction.services.crawl.CrawlDataService;
-import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Random;
 
 @SpringBootApplication
@@ -41,6 +36,7 @@ import java.util.Random;
 @ConfigurationPropertiesScan("com.swp391.JewelryProduction.*")
 @EnableJpaRepositories("com.swp391.JewelryProduction.*")
 @EnableScheduling
+@EnableTransactionManagement
 public class Application   {
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
@@ -82,6 +78,14 @@ public class Application   {
 						.role(Role.ADMIN)
 						.status(AccountStatus.ACTIVE)
 						.build();
+				acc.setUserInfo(UserInfo.builder()
+								.firstName("Dung")
+								.lastName("Nguyen Hoang")
+								.gender(Gender.MALE)
+								.address("Vinhomes")
+								.birthDate(LocalDate.parse("2004-10-11"))
+								.phoneNumber("0916320563")
+						.build());
 				accountRepository.save(acc);
 
 				acc = Account.builder()
@@ -91,19 +95,37 @@ public class Application   {
 						.role(Role.MANAGER)
 						.status(AccountStatus.ACTIVE)
 						.build();
+			acc.setUserInfo(UserInfo.builder()
+					.firstName("Dung")
+					.lastName("Nguyen Hoang")
+					.gender(Gender.MALE)
+					.address("Vinhomes")
+					.birthDate(LocalDate.parse("2004-10-11"))
+					.phoneNumber("0916320563")
+					.build());
 				accountRepository.save(acc);
 
 				Faker faker = new Faker();
 				Random rand = new Random();
 				for (int i = 0; i < 20; i++) {
-					Account account = Account.builder()
+					acc = Account.builder()
 							.email(faker.internet().safeEmailAddress())
 							.password(passwordEncoder.encode(faker.internet().password()))
 							.dateCreated(LocalDateTime.now().plusWeeks(rand.nextLong(10)).plusDays(rand.nextLong(31)))
 							.role(Role.CUSTOMER)
 							.status(AccountStatus.ACTIVE)
 							.build();
-					accountRepository.save(account);
+					acc.setUserInfo(UserInfo.builder()
+							.firstName(faker.name().firstName())
+							.lastName(faker.name().lastName())
+							.gender(Gender.values()[rand.nextInt(1)])
+							.address(faker.address().fullAddress())
+							.birthDate(faker.date().birthday(10, 80).toInstant()
+									.atZone(ZoneId.systemDefault())
+									.toLocalDate())
+							.phoneNumber(faker.phoneNumber().phoneNumber())
+							.build());
+					accountRepository.save(acc);
 				}
 
 				acc = Account.builder()
@@ -113,6 +135,14 @@ public class Application   {
 						.role(Role.ADMIN)
 						.status(AccountStatus.ACTIVE)
 						.build();
+			acc.setUserInfo(UserInfo.builder()
+					.firstName("Khai")
+					.lastName("Tran Mai Quang")
+					.gender(Gender.MALE)
+					.address("Vinhomes")
+					.birthDate(LocalDate.parse("2004-10-11"))
+					.phoneNumber("0916320563")
+					.build());
 				accountRepository.save(acc);
 		};
 	}
