72899603cdc26691c33e2e2bcb411e0bd08d552f
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-06-25 12:49:40 +0700

    Add Eager fetch type for staffOrderHistory when findById

--- Back-end/src/main/java/com/swp391/JewelryProduction/repositories/OrderRepository.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/repositories/OrderRepository.java
@@ -1,7 +1,15 @@
 package com.swp391.JewelryProduction.repositories;
 
 import com.swp391.JewelryProduction.pojos.Order;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderRepository extends JpaRepository<Order, String> {
+    @NotNull
+    @EntityGraph( attributePaths = {"staffOrderHistory"} )
+    @Override
+    Optional<Order> findById(@NotNull String s);
 }
