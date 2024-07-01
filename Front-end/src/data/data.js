d635baba6fa598b7ddaa405abea1794e278c1b79
Bùi Khánh Duy <buikhanhduy116@gmail.com>
2024-07-01 22:09:57 +0700

    Create data.js
    data

---
+++ Front-end/src/data/data.js
@@ -0,0 +1,22 @@
+export const roles = [
+  {
+    name: "All",
+    id: "0",
+  },
+  {
+    name: "Admin",
+    id: "0",
+  },
+  {
+    name: "Manager",
+    id: "0",
+  },
+  {
+    name: "Sales Staff",
+    id: "0",
+  },
+  {
+    name: "Contribution",
+    id: "0",
+  },
+];
