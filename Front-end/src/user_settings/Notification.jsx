3b89949e957ac6e3d6dca5b4f55d68449350a48f
datvtse180371 <datvtse180371@fpt.edu.vn>
2024-07-11 05:01:13 +0700

    Update Notification.jsx

--- Front-end/src/user_settings/Notification.jsx
+++ Front-end/src/user_settings/Notification.jsx
@@ -4,8 +4,9 @@ import axios from "axios";
 import ServerUrl from "../reusable/ServerUrl";
 import { jwtDecode } from "jwt-decode";
 import { useAuth } from "../provider/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
-function NotificationPage() {
+export function NotificationPage() {
   const [data, setData] = useState();
   const { token } = useAuth();
 
@@ -53,4 +54,41 @@ function NotificationPage() {
   );
 }
 
-export default NotificationPage;
+export const NotificationDetail = () => {
+  const [notification, setNotification] = useState([]);
+  const navigator = useNavigate();
+  const [token] = useAuth();
+
+  useEffect(() => {
+    if (token) {
+      const decodedToken = jwtDecode(token);
+
+      axios({
+        method: "GET",
+        url: `${ServerUrl}/api/notification/${decodedToken.id}/get/${notificationId}`,
+        headers: { "Content-Type": "Application/json" },
+      }).then((res) => setNotification(res.data));
+    }
+  }, [token]);
+
+  function acceptedReport() {
+    navigator("/${orderId}/confirm?confirmed=true")
+  }
+
+  function declinedReport() {
+    navigator("/${orderId}/confirm?confirmed=false")
+  }
+
+  return (
+    <div>
+      <div>
+        <h2>{notification.report.title}</h2>
+        <h3>{notification.report.type}</h3>
+        <h3>{notification.report.sender}</h3>
+        <p>{notification.report.description}</p>
+        <button onClick={acceptedReport} className="btn btn-success">Accept</button>
+        <button onClick={declinedReport} className="btn btn-danger">Decline</button>
+      </div>
+    </div>
+  );
+}
