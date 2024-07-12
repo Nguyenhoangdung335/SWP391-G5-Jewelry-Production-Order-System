307a4cc8cdf0286b5fbea059c205f4f417e47517
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-07-12 13:08:25 +0700

    Refractor ProductController

--- Back-end/src/main/java/com/swp391/JewelryProduction/controller/ProductController.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/controller/ProductController.java
@@ -1,26 +1,35 @@
 package com.swp391.JewelryProduction.controller;
 
+import com.swp391.JewelryProduction.dto.ProductDTO;
 import com.swp391.JewelryProduction.pojos.designPojos.Product;
 import com.swp391.JewelryProduction.pojos.designPojos.ProductSpecification;
 import com.swp391.JewelryProduction.services.product.ProductService;
 import com.swp391.JewelryProduction.util.Response;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/product")
+@RequestMapping("api/products")
 public class ProductController {
     private final ProductService productService;
 
-    @GetMapping("/get-product-list")
-    public ResponseEntity<Response> getProducts() {
+    //<editor-fold desc="PRODUCT ENDPOINTS" defaultstate="collapsed">
+    @GetMapping("")
+    public ResponseEntity<Response> getProducts(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "5") int size
+    ) {
+        Page<ProductDTO> productPage = productService.findAll(page, size);
         return Response.builder()
                 .status(HttpStatus.OK)
-                .message("Request send successfully.")
-                .response("productList", productService.findAll())
+                .message("Get products successfully")
+                .response("products", productPage.getContent())
+                .response("totalPages", productPage.getTotalPages())
+                .response("totalElements", productPage.getTotalElements())
                 .buildEntity();
     }
 
@@ -32,7 +41,7 @@ public class ProductController {
                 .buildEntity();
     }
 
-    @DeleteMapping("/{productId}/remove")
+    @DeleteMapping("/{productId}")
     public ResponseEntity<Response> removeProduct(@PathVariable String productId) {
         productService.deleteProduct(productId);
         return Response.builder()
@@ -41,7 +50,7 @@ public class ProductController {
                 .buildEntity();
     }
 
-    @PostMapping("/create")
+    @PostMapping("")
     public ResponseEntity<Response> createProduct(@RequestBody Product product) {
         productService.saveProduct(product);
         return Response.builder()
@@ -49,30 +58,63 @@ public class ProductController {
                 .message("Request send successfully.")
                 .buildEntity();
     }
+    //</editor-fold>
 
-    @PostMapping("/customize")
-    public ResponseEntity<Response> saveCustomization(@RequestBody ProductSpecification specs) {
+    //<editor-fold desc="PRODUCT SPECIFICATION ENDPOINTS" defaultstate="collapsed">
+    @GetMapping("/customize")
+    public ResponseEntity<Response> getAllSpecification (
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "5") int size
+    ) {
+        Page<ProductSpecification> specificationPage = productService.findAllSpecification(page, size);
         return Response.builder()
-                .status(HttpStatus.OK)
-                .message("Request send successfully.")
-                .response("productSpecification", productService.saveSpecification(specs))
+                .message("Get all Specifications successully")
+                .response("specifications", specificationPage.getContent())
+                .response("totalPages", specificationPage.getTotalPages())
+                .response("totalElements", specificationPage.getTotalElements())
                 .buildEntity();
     }
 
-    @PostMapping("/remove/{specificationId}")
+    @GetMapping("/customize/{specificationId}")
+    public ResponseEntity<Response> getSpecificationById (
+            @PathVariable int specificationId
+    ) {
+        ProductSpecification specs = productService.findProductSpecificationById(specificationId);
+        return Response.builder()
+                .message("Get Specification with id "+specs.getId()+" successfully")
+                .response("productSpecification", specs)
+                .buildEntity();
+    }
+
+    @PostMapping("/customize")
+    public ResponseEntity<Response> saveCustomization(@RequestBody ProductSpecification specs) {
+        specs = productService.saveSpecification(specs);
+        return Response.builder()
+                .status(HttpStatus.OK)
+                .message("Create Specification with "+specs.getId()+" successfully")
+                .response("productSpecification", specs)
+                .buildEntity();
+    }
+
+    @PutMapping("/customize/{specificationId}")
+    public ResponseEntity<Response> updateCustomization(
+            @PathVariable int specificationId,
+            @RequestBody ProductSpecification specs
+    ) {
+        specs = productService.updateSpecification(specificationId ,specs);
+        return Response.builder()
+                .message("Update Specification with id "+specs.getId()+" successfully")
+                .response("productSpecification", specs)
+                .buildEntity();
+    }
+
+    @DeleteMapping("/customize/{specificationId}")
     public ResponseEntity<Response> deleteCustomization(@PathVariable int specificationId) {
         productService.deleteSpecificationById(specificationId);
         return Response.builder()
                 .status(HttpStatus.OK)
-                .message("Request send successfully.")
+                .message("Delete Specification with id "+specificationId+" successfully")
                 .buildEntity();
     }
-
-    @GetMapping("/get-all/specification")
-    public ResponseEntity<Response> getAllSpecification () {
-        return Response.builder()
-                .message("Request send successfully")
-                .response("list", productService.findAllSpecification())
-                .buildEntity();
-    }
+    //</editor-fold>
 }
