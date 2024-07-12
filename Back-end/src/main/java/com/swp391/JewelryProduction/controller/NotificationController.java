972de34da12a160ec021081253557a50e0ede93a
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-07-12 14:53:03 +0700

    Update Notification endpoints

--- Back-end/src/main/java/com/swp391/JewelryProduction/controller/NotificationController.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/controller/NotificationController.java
@@ -1,14 +1,12 @@
 package com.swp391.JewelryProduction.controller;
 
 import com.swp391.JewelryProduction.pojos.Notification;
-import com.swp391.JewelryProduction.services.account.AccountService;
-import com.swp391.JewelryProduction.enums.ConfirmedState;
 import com.swp391.JewelryProduction.services.notification.NotificationService;
 import com.swp391.JewelryProduction.services.report.ReportService;
 import com.swp391.JewelryProduction.util.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,26 +16,54 @@ import java.util.UUID;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/notification")
+@RequestMapping("/api/notifications")
 public class NotificationController {
     private final NotificationService notificationService;
     private final ReportService reportService;
 
-    @GetMapping("/{accountId}/get-all")
-    public ResponseEntity<Response> getAllNotifications(@PathVariable("accountId") String receiverId) {
+    @GetMapping("/receiver/{receiverId}")
+    public ResponseEntity<Response> getAllNotifications(
+            @PathVariable("receiverId") String receiverId,
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size
+    ) {
+        Page<Notification> notificationPage = notificationService.findAllByReceiverId(receiverId, page, size);
+
         return Response.builder()
                 .status(HttpStatus.OK)
-                .message("Request send successfully.")
-                .response("notificationList", notificationService.findAllByReceiver_Id(receiverId))
+                .message("Get notifications in page "+page+" successfully")
+                .response("notifications", notificationPage.getContent())
+                .response("totalPages", notificationPage.getTotalElements())
+                .response("totalElements", notificationPage.getTotalElements())
                 .buildEntity();
     }
 
-    @GetMapping("/get/{notificationId}")
-    public ResponseEntity<Response> getNotification(@PathVariable("notificationId") UUID notificationId) {
+    @GetMapping("/{notificationId}")
+    public ResponseEntity<Response> getNotification(
+            @PathVariable("notificationId") Integer notificationId
+    ) {
+        Notification notification = notificationService.getNotificationById(notificationId);
         return Response.builder()
                 .status(HttpStatus.OK)
-                .message("Request send successfully")
-                .response("notification", notificationService.getNotificationById(notificationId))
+                .message("Get notification with id "+notification.getId()+" successfully")
+                .response("notification", notification)
+                .buildEntity();
+    }
+
+    @GetMapping("/receiver/{receiverId}/unread")
+    public ResponseEntity<Response> getUnreadNotifications (
+            @PathVariable("receiverId") String receiverId,
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size
+    ) {
+        Page<Notification> notificationPage = notificationService.findAllByReceiverIdAndUnread(receiverId, page, size);
+
+        return Response.builder()
+                .status(HttpStatus.OK)
+                .message("Get notifications in page "+page+" successfully")
+                .response("notifications", notificationPage.getContent())
+                .response("totalPages", notificationPage.getTotalElements())
+                .response("totalElements", notificationPage.getTotalElements())
                 .buildEntity();
     }
 
