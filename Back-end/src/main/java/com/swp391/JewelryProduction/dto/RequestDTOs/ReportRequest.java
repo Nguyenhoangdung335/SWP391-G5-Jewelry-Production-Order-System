0a555277a34a5c633422c93fded72afad6659649
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-07-13 16:36:30 +0700

    Add additional validation for reportRequest

--- Back-end/src/main/java/com/swp391/JewelryProduction/dto/RequestDTOs/ReportRequest.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/dto/RequestDTOs/ReportRequest.java
@@ -22,13 +22,13 @@ public class ReportRequest {
     @NotEmpty(message = "The request must contain a description content")
     @NotBlank(message = "The request description must not be blank")
     private String description;
-    @EnumNameValidator(regexp = "REQUEST|QUOTATION|DESIGN|NONE", message = "Report Type of assigned one")
+    @EnumNameValidator(regexp = "REQUEST|QUOTATION|DESIGN|NONE|FINISHED_PRODUCT", message = "Report Type of assigned one")
     @NotNull
     private ReportType type;
     private String senderId;
     //Contain id for Specification id, Quotation id, Design id
     @NotEmpty(message = "The request must contain the contentID, must assign the id for either Specification id, quotation id, or design id")
     @NotBlank(message = "The request contentID must not be blank, must assign the id for either Specification id, quotation id, or design id")
-    @Pattern(regexp = "^(QUO|DES)\\d{5}$|\\d+", message = "The report content ID must start with PRO, QUO, or DES followed by exactly 5 digits")
+    @Pattern(regexp = "^(QUO|DES|PRO)\\d{5}$|\\d+", message = "The report content ID must start with PRO, QUO, or DES followed by exactly 5 digits")
     private String reportContentID;
 }
