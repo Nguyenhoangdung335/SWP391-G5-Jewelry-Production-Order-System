ada62669850bb39b1c5bb75eb34752ade16c3401
datvtse180371 <datvtse180371@fpt.edu.vn>
2024-07-16 10:59:42 +0700

    Update MaterialRepository.java

--- Back-end/src/main/java/com/swp391/JewelryProduction/repositories/MaterialRepository.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/repositories/MaterialRepository.java
@@ -1,10 +1,10 @@
 package com.swp391.JewelryProduction.repositories;
 
-import com.swp391.JewelryProduction.pojos.Material;
+import com.swp391.JewelryProduction.pojos.Price.MetalPrice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MaterialRepository extends JpaRepository<Material, Long> {
-    Optional<Material> findByName(String name);
+public interface MaterialRepository extends JpaRepository<MetalPrice, Long> {
+    Optional<MetalPrice> findByName(String name);
 }
