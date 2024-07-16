4acc51a63c0fe6546efd2052df928cde2a115094
datvtse180371 <datvtse180371@fpt.edu.vn>
2024-07-16 10:59:16 +0700

    Create GemstoneShape.java

---
+++ Back-end/src/main/java/com/swp391/JewelryProduction/enums/GemstoneShape.java
@@ -0,0 +1,21 @@
+package com.swp391.JewelryProduction.enums;
+
+public enum GemstoneShape {
+    ROUND,
+    RADIANT,
+    SQUARE_RADIANT,
+    PEAR,
+    MARQUISE,
+    BRIOLETTE,
+    ASSCHER,
+    BAGUETTE,
+    PRINCESS,
+    CUSHION,
+    SQUARE_CUSHION,
+    OVAL,
+    KITE,
+    HEART,
+    TRILLION,
+    HALF_MOON,
+    TRAPEZOID
+}
