c5f14e366770932cc0c541160a6407170a066f9a
datvtse180371 <datvtse180371@fpt.edu.vn>
2024-07-16 11:00:01 +0700

    Update ICrawlDataService.java

--- Back-end/src/main/java/com/swp391/JewelryProduction/services/crawl/ICrawlDataService.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/services/crawl/ICrawlDataService.java
@@ -1,6 +1,6 @@
 package com.swp391.JewelryProduction.services.crawl;
 
-import com.swp391.JewelryProduction.pojos.Material;
+import com.swp391.JewelryProduction.pojos.Price.MetalPrice;
 import org.springframework.http.codec.ServerSentEvent;
 import reactor.core.publisher.Flux;
 
@@ -10,9 +10,6 @@ import java.util.List;
 public interface ICrawlDataService {
 
     void crawData() throws IOException, InterruptedException;
-    Flux<ServerSentEvent<List<Material>>> getAll();
-    void addMaterial(Material material);
-    void deleteMaterial(Long id);
-    void updateMaterial(Material material);
+    Flux<ServerSentEvent<List<MetalPrice>>> getAll();
 
 }
