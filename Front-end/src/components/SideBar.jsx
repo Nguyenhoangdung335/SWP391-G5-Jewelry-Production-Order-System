ed745fb70fd4735ce35016fc117bb04dfb6cc64a
Khai-Tran24 <163870078+Khai-Tran24@users.noreply.github.com>
2024-07-17 12:29:34 +0700

    Update Header and SideBar Show

--- Front-end/src/components/SideBar.jsx
+++ Front-end/src/components/SideBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../provider/AuthProvider";
 
 function SideBar() {
-  const { setToken } = useAuth();
+  const { token, setToken } = useAuth();
 
   const handleLogout = () => {
     localStorage.removeItem("token");
@@ -15,7 +15,7 @@ function SideBar() {
       className="w-100 position-sticky"
       style={{
         height: "100vh",
-        backgroundColor:"rgb(62, 62, 62)",
+        backgroundColor: "rgb(62, 62, 62)",
       }}
     >
       <div className="text-center p-4 text-decoration-none">
@@ -33,16 +33,18 @@ function SideBar() {
           </Link>
         </div>
       </div>
-      <div className=" side-bar-item side-bar-ltr">
-        <div style={{ padding: "15px 30px 9px" }}>
-          <Link
-            to="/user_setting_page/order_history_page"
-            className="text-decoration-none text-white"
-          >
-            <h5>Order History</h5>
-          </Link>
+      {token === "CUSTOMER" && (
+        <div className=" side-bar-item side-bar-ltr">
+          <div style={{ padding: "15px 30px 9px" }}>
+            <Link
+              to="/user_setting_page/order_history_page"
+              className="text-decoration-none text-white"
+            >
+              <h5>Order History</h5>
+            </Link>
+          </div>
         </div>
-      </div>
+      )}
       <div className=" side-bar-item side-bar-ltr">
         <div style={{ padding: "15px 30px 9px" }}>
           <Link
