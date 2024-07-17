70a618673abba11a9918eea902cc4a207119e9c9
Khai-Tran24 <163870078+Khai-Tran24@users.noreply.github.com>
2024-07-17 14:25:55 +0700

    Update ClientManager.jsx
    Update UI

--- Front-end/src/clientManager/ClientManager.jsx
+++ Front-end/src/clientManager/ClientManager.jsx
@@ -4,8 +4,8 @@ import { FiPlus } from "react-icons/fi";
 import axios from "axios";
 import { roles } from "../data/Roles";
 import ServerUrl from "../reusable/ServerUrl";
-import { FaTrash } from 'react-icons/fa';
-import { FaEdit } from 'react-icons/fa';
+import { FaTrash } from "react-icons/fa";
+import { FaEdit } from "react-icons/fa";
 
 export default function ClientManager() {
   const [filterRole, setFilterRole] = useState("CUSTOMER");
@@ -21,9 +21,14 @@ export default function ClientManager() {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios.get(`${ServerUrl}/api/admin/get/account/${currentPage - 1}?role=${filterRole}`, {
-          headers: { "Content-Type": "application/json" },
-        });
+        const res = await axios.get(
+          `${ServerUrl}/api/admin/get/account/${
+            currentPage - 1
+          }?role=${filterRole}`,
+          {
+            headers: { "Content-Type": "application/json" },
+          }
+        );
         setData(res.data.responseList.accounts);
         setTotalPages(res.data.responseList.totalPages);
       } catch (err) {
@@ -72,18 +77,24 @@ export default function ClientManager() {
     };
 
     try {
-      const res = await axios.put(`${ServerUrl}/api/admin/update/account`, values, {
-        headers: { "Content-Type": "application/json" },
-      });
-      console.log('Update Response:', res.data);
+      const res = await axios.put(
+        `${ServerUrl}/api/admin/update/account`,
+        values,
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      console.log("Update Response:", res.data);
 
       // Cập nhật trạng thái dữ liệu trên client sau khi chỉnh sửa thành công
-      const updatedData = data.map((item) => (item.id === values.id ? values : item));
+      const updatedData = data.map((item) =>
+        item.id === values.id ? values : item
+      );
       setData(updatedData);
       setIsModalVisible(false);
       setSelectedUser(null);
     } catch (err) {
-      console.error('Error updating account:', err);
+      console.error("Error updating account:", err);
     }
   };
 
@@ -107,14 +118,18 @@ export default function ClientManager() {
     };
 
     try {
-      const res = await axios.post(`${ServerUrl}/api/admin/create/account`, newEmployee, {
-        headers: { "Content-Type": "application/json" },
-      });
-      console.log('Add Response:', res.data.responseList.account);
+      const res = await axios.post(
+        `${ServerUrl}/api/admin/create/account`,
+        newEmployee,
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      console.log("Add Response:", res.data.responseList.account);
       setData([...data, res.data.responseList.account]); // Cập nhật trạng thái dữ liệu trên client sau khi thêm thành công
       setIsAddModalVisible(false);
     } catch (err) {
-      console.error('Error adding account:', err);
+      console.error("Error adding account:", err);
     }
   };
 
@@ -129,16 +144,19 @@ export default function ClientManager() {
 
   const handleConfirmDelete = async () => {
     try {
-      const res = await axios.delete(`${ServerUrl}/api/admin/delete/account?accountId=${deleteUser}`, {
-        headers: { "Content-Type": "application/json" },
-      });
-      console.log('Delete Response:', res.data);
+      const res = await axios.delete(
+        `${ServerUrl}/api/admin/delete/account?accountId=${deleteUser}`,
+        {
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      console.log("Delete Response:", res.data);
 
       // Cập nhật trạng thái dữ liệu trên client sau khi xóa thành công
       const updatedData = data.filter((item) => item.id !== deleteUser);
       setData(updatedData);
     } catch (err) {
-      console.log('Error deleting account:', err);
+      console.log("Error deleting account:", err);
     }
     setDeleteModalVisible(false);
     setDeleteUser(null);
@@ -185,7 +203,10 @@ export default function ClientManager() {
 
   const formatDate = (dateArray) => {
     const [year, month, day] = dateArray;
-    return `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
+    return `${year}-${String(month).padStart(2, "0")}-${String(day).padStart(
+      2,
+      "0"
+    )}`;
   };
 
   return (
@@ -236,8 +257,12 @@ export default function ClientManager() {
           margin-left: auto;
         }
       `}</style>
-      <p style={{ margin: 0, fontSize: 24, fontWeight: "bold" }}>Welcome, K!</p>
-      <p style={{ fontSize: 16 }}>Client Manager</p>
+      <div className="pb-2">
+        <p style={{ margin: 0, fontSize: 24, fontWeight: "bold" }}>
+          Welcome, Admin!
+        </p>
+        <p style={{ fontSize: 18 }}>Client Manager</p>
+      </div>
       <div
         style={{
           display: "flex",
@@ -247,14 +272,9 @@ export default function ClientManager() {
           marginBottom: "2%",
         }}
       >
-        <Button
-          variant="secondary"
-          className="add-client-button"
-          onClick={handleAddClick}
-          style={{ marginLeft: "1px" }} 
-        >
-          <FiPlus color="rgba(224, 215, 234, 1)" />
-          Add Client
+        <Button variant="primary" onClick={handleAddClick}>
+          <FiPlus size={20} />
+          <span className="ms-1">Add Client</span>
         </Button>
       </div>
 
@@ -291,10 +311,13 @@ export default function ClientManager() {
               </td>
               <td>
                 <Button variant="link" onClick={() => handleEdit(item)}>
-                <FaEdit />
+                  <FaEdit size={20} />
                 </Button>
-                <Button variant="link" onClick={() => handleDeleteClick(item.id)}>
-                <FaTrash />
+                <Button
+                  variant="link"
+                  onClick={() => handleDeleteClick(item.id)}
+                >
+                  <FaTrash size={20} />
                 </Button>
               </td>
             </tr>
@@ -337,13 +360,13 @@ export default function ClientManager() {
       </div>
 
       <Modal show={isModalVisible} onHide={handleCancel} centered>
-        <Modal.Header >
+        <Modal.Header>
           <Modal.Title>Edit Client</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           {selectedUser && (
             <Form onSubmit={handleSave}>
-              <Form.Group controlId="role">
+              <Form.Group controlId="role" className="mb-3">
                 <Form.Label>Role</Form.Label>
                 <Form.Control
                   as="select"
@@ -355,7 +378,7 @@ export default function ClientManager() {
                 </Form.Control>
               </Form.Group>
 
-              <Row>
+              <Row className="mb-3">
                 <Col>
                   <Form.Group controlId="firstName">
                     <Form.Label>First Name</Form.Label>
@@ -378,7 +401,7 @@ export default function ClientManager() {
                 </Col>
               </Row>
 
-              <Form.Group controlId="gmail">
+              <Form.Group controlId="gmail" className="mb-3">
                 <Form.Label>Email</Form.Label>
                 <Form.Control
                   type="email"
@@ -387,7 +410,7 @@ export default function ClientManager() {
                 />
               </Form.Group>
 
-              <Form.Group controlId="phone">
+              <Form.Group controlId="phone" className="mb-3">
                 <Form.Label>Phone</Form.Label>
                 <Form.Control
                   type="text"
@@ -395,29 +418,32 @@ export default function ClientManager() {
                   required
                 />
               </Form.Group>
-
-              <Form.Group controlId="birthDate">
-                <Form.Label>Birth Date</Form.Label>
-                <Form.Control
-                  type="date"
-                  defaultValue={formatDate(selectedUser.userInfo.birthDate)}
-                  required
-                />
-              </Form.Group>
-
-              <Form.Group controlId="gender">
-                <Form.Label>Gender</Form.Label>
-                <Form.Control
-                  as="select"
-                  defaultValue={selectedUser.userInfo.gender}
-                  required
-                >
-                  <option value="MALE">Male</option>
-                  <option value="FEMALE">Female</option>
-                </Form.Control>
-              </Form.Group>
-
-              <Form.Group controlId="address">
+              <Row>
+                <Col>
+                  <Form.Group controlId="birthDate" className="mb-3">
+                    <Form.Label>Birth Date</Form.Label>
+                    <Form.Control
+                      type="date"
+                      defaultValue={formatDate(selectedUser.userInfo.birthDate)}
+                      required
+                    />
+                  </Form.Group>
+                </Col>
+                <Col>
+                  <Form.Group controlId="gender" className="mb-3">
+                    <Form.Label>Gender</Form.Label>
+                    <Form.Control
+                      as="select"
+                      defaultValue={selectedUser.userInfo.gender}
+                      required
+                    >
+                      <option value="MALE">Male</option>
+                      <option value="FEMALE">Female</option>
+                    </Form.Control>
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Form.Group controlId="address" className="mb-3">
                 <Form.Label>Address</Form.Label>
                 <Form.Control
                   type="text"
@@ -427,11 +453,15 @@ export default function ClientManager() {
               </Form.Group>
 
               <div className="d-flex justify-content-between">
+                <Button
+                  variant="secondary"
+                  onClick={handleCancel}
+                  className="mt-3"
+                >
+                  Back
+                </Button>
                 <Button variant="primary" type="submit" className="mt-3">
                   Save Changes
-                </Button>
-                <Button variant="secondary" onClick={handleCancel} className="mt-3">
-                  Back
                 </Button>
               </div>
             </Form>
@@ -439,20 +469,24 @@ export default function ClientManager() {
         </Modal.Body>
       </Modal>
 
-      <Modal show={isAddModalVisible} onHide={() => setIsAddModalVisible(false)} centered>
+      <Modal
+        show={isAddModalVisible}
+        onHide={() => setIsAddModalVisible(false)}
+        centered
+      >
         <Modal.Header>
           <Modal.Title>Add Client</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form onSubmit={handleAdd}>
             {/* Hiển thị trường Role với CUSTOMER mặc định và disabled */}
-            <Form.Group controlId="role">
+            <Form.Group controlId="role" className="mb-3">
               <Form.Label>Role</Form.Label>
               <Form.Control as="select" defaultValue="CUSTOMER" disabled>
                 <option value="CUSTOMER">CUSTOMER</option>
               </Form.Control>
             </Form.Group>
-            <Row>
+            <Row className="mb-3">
               <Col>
                 <Form.Group controlId="firstName">
                   <Form.Label>First Name</Form.Label>
@@ -467,40 +501,46 @@ export default function ClientManager() {
               </Col>
             </Row>
 
-            <Form.Group controlId="gmail">
+            <Form.Group controlId="gmail" className="mb-3">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" required />
             </Form.Group>
 
-            <Form.Group controlId="phone">
+            <Form.Group controlId="phone" className="mb-3">
               <Form.Label>Phone</Form.Label>
               <Form.Control type="text" required />
             </Form.Group>
-
-            <Form.Group controlId="birthDate">
-              <Form.Label>Birth Date</Form.Label>
-              <Form.Control type="date" required />
-            </Form.Group>
-
-            <Form.Group controlId="gender">
-              <Form.Label>Gender</Form.Label>
-              <Form.Control as="select" required>
-                <option value="MALE">Male</option>
-                <option value="FEMALE">Female</option>
-              </Form.Control>
-            </Form.Group>
-
-            <Form.Group controlId="address">
+            <Row>
+              <Col>
+                <Form.Group controlId="birthDate" className="mb-3">
+                  <Form.Label>Birth Date</Form.Label>
+                  <Form.Control type="date" required />
+                </Form.Group>
+              </Col>
+              <Col>
+                <Form.Group controlId="gender" className="mb-3">
+                  <Form.Label>Gender</Form.Label>
+                  <Form.Control as="select" required>
+                    <option value="MALE">Male</option>
+                    <option value="FEMALE">Female</option>
+                  </Form.Control>
+                </Form.Group>
+              </Col>
+            </Row>
+            <Form.Group controlId="address" className="mb-3">
               <Form.Label>Address</Form.Label>
               <Form.Control type="text" required />
             </Form.Group>
-
             <div className="d-flex justify-content-between">
+              <Button
+                variant="secondary"
+                onClick={() => setIsAddModalVisible(false)}
+                className="mt-2"
+              >
+                Back
+              </Button>
               <Button variant="primary" type="submit" className="mt-2">
                 Add Client
-              </Button>
-              <Button variant="secondary" onClick={() => setIsAddModalVisible(false)} className="mt-2">
-                Back
               </Button>
             </div>
           </Form>
@@ -508,12 +548,12 @@ export default function ClientManager() {
       </Modal>
 
       <Modal show={deleteModalVisible} onHide={handleCancelDelete} centered>
-        <Modal.Header >
+        <Modal.Header>
           <Modal.Title>Confirm Delete</Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <p>Are you sure you want to delete this client?</p>
-          <div className="d-flex justify-content-end">
+          <div className="d-flex justify-content-around">
             <Button variant="secondary" onClick={handleCancelDelete}>
               Cancel
             </Button>
@@ -522,7 +562,7 @@ export default function ClientManager() {
               onClick={handleConfirmDelete}
               className="ms-2"
             >
-              <FaTrash />
+              Delete
             </Button>
           </div>
         </Modal.Body>
