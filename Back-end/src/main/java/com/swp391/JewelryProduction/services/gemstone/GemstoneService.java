c000d25bc815c879d8fef42f41cc83dda3560f67
datvtse180371 <datvtse180371@fpt.edu.vn>
2024-07-19 02:05:46 +0700

    Update GemstoneService.java

--- Back-end/src/main/java/com/swp391/JewelryProduction/services/gemstone/GemstoneService.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/services/gemstone/GemstoneService.java
@@ -1,6 +1,6 @@
 package com.swp391.JewelryProduction.services.gemstone;
 
-import com.swp391.JewelryProduction.pojos.gemstone.Gemstone;
+import com.swp391.JewelryProduction.pojos.gemstone.*;
 
 import java.util.List;
 import java.util.Map;
@@ -11,8 +11,18 @@ public interface GemstoneService {
     double calculatePrice(Gemstone gemstone);
 
     List<Gemstone> getGemstones();
+    List<ShapeMultiplier> getShapeMultipliers();
+    List<CutMultiplier> getCutMultipliers();
+    List<ColorMultiplier> getColorMultipliers();
+    List<ClarityMultiplier> getClarityMultipliers();
+
     Gemstone getGemstone(long id);
     Gemstone createGemstone(Gemstone gemstone);
     Gemstone updateGemstone(Gemstone gemstone);
     boolean deleteGemstone(long id);
+
+    CutMultiplier updateCutMultiplier(long id, double multiplier);
+    ColorMultiplier updateColorMultiplier(long id, double multiplier);
+    ClarityMultiplier updateClarityMultiplier(long id, double multiplier);
+    ShapeMultiplier updateShapeMultiplier(long id, double multiplier);
 }
