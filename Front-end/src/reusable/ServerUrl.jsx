9891d8c08ce2deca75d2751f18797037b002ed4b
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-07-23 06:35:00 +0700

    Update to using deployed server url

--- Front-end/src/reusable/ServerUrl.jsx
+++ Front-end/src/reusable/ServerUrl.jsx
@@ -1,4 +1,4 @@
-// const ServerUrl = "https://swp391-g5-jewelry-production-order-system.onrender.com";
-const ServerUrl = "http://localhost:8080";
+const ServerUrl = "https://swp391-g5-jewelry-production-order-system.onrender.com";
+// const ServerUrl = "http://localhost:8080";
 
 export default ServerUrl;
