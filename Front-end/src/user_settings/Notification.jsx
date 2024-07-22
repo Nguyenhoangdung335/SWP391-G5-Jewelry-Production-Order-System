8dcf7c27238133119450370a977c9c3ecf46390c
okiyata <vietthaivipo1234@gmail.com>
2024-07-23 04:53:19 +0700

    Front-end - No orders/notifications found

--- Front-end/src/user_settings/Notification.jsx
+++ Front-end/src/user_settings/Notification.jsx
@@ -1,88 +1,88 @@
-import React, { useEffect, useState } from "react";
-import { useAuth } from "../provider/AuthProvider";
+import React, {useEffect, useState} from "react";
+import {useAuth} from "../provider/AuthProvider";
 import ServerUrl from "../reusable/ServerUrl";
 import axios from "axios";
-import { jwtDecode } from "jwt-decode";
-import { Link } from "react-router-dom";
-import { Col, Container, Row } from "react-bootstrap";
+import {jwtDecode} from "jwt-decode";
+import {Link} from "react-router-dom";
+import {Col, Container, Row} from "react-bootstrap";
 import arrayToDate from "../reusable/ArrayToDate";
 
-const HoverDiv = ({ children }) => {
-  const [mouseEnter, setMouseEnter] = useState(false);
+const HoverDiv = ({children}) => {
+    const [mouseEnter, setMouseEnter] = useState(false);
 
-  const handleMouse = (e) => {
-    setMouseEnter(e);
-  };
-  return (
-    <div
-      className="p-3 m-3 text-black shadow-sm"
-      onMouseEnter={() => handleMouse(true)}
-      onMouseLeave={() => handleMouse(false)}
-      style={{
-        border: "1px solid #c4c4c4",
-        borderRadius: "20px",
-        transform: mouseEnter ? "scale(0.99)" : "scale(0.98)",
-        transition: "transform 200ms ease-out",
-      }}
-    >
-      {children}
-    </div>
-  );
+    const handleMouse = (e) => {
+        setMouseEnter(e);
+    };
+    return (
+        <div
+            className="p-3 m-3 text-black shadow-sm"
+            onMouseEnter={() => handleMouse(true)}
+            onMouseLeave={() => handleMouse(false)}
+            style={{
+                border: "1px solid #c4c4c4",
+                borderRadius: "20px",
+                transform: mouseEnter ? "scale(0.99)" : "scale(0.98)",
+                transition: "transform 200ms ease-out",
+            }}
+        >
+            {children}
+        </div>
+    );
 };
 
 const NotificationPage = () => {
-  const [notifications, setNotifications] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const { token } = useAuth();
-  const decodedToken = jwtDecode(token);
+    const [notifications, setNotifications] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const {token} = useAuth();
+    const decodedToken = jwtDecode(token);
 
-  useEffect(() => {
-    axios
-      .get(`${ServerUrl}/api/notifications/receiver/${decodedToken.id}`)
-      .then((response) => {
-        if (response.data.status === "OK") {
-          setNotifications(response.data.responseList.notifications);
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching notifications:", error);
-      })
-      .finally(() => setLoading(false));
+    useEffect(() => {
+        axios
+            .get(`${ServerUrl}/api/notifications/receiver/${decodedToken.id}`)
+            .then((response) => {
+                if (response.data.status === "OK") {
+                    setNotifications(response.data.responseList.notifications);
+                }
+            })
+            .catch((error) => {
+                console.error("Error fetching notifications:", error);
+            })
+            .finally(() => setLoading(false));
 
-    return () => {
-      
-    };
-  }, [decodedToken.id]);
+        return () => {
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+        };
+    }, [decodedToken.id]);
 
-  return (
-    <Container>
-      {notifications.length === 0 ? (
-        <Row className="d-flex justify-content-center align-items-center mt-5">
-          <Col xs="auto" className="text-center">
-            <h4>No notifications</h4>
-          </Col>
-        </Row>
-      ) : (
-        notifications.map((notification) => (
-          <Link
-            key={notification.id}
-            to={`${notification.id}`}
-            style={{ textDecoration: "none" }}
-          >
-            <HoverDiv>
-              <h4>{notification.report.title}</h4>
-              <p>{notification.report.description}</p>
-              <p>{arrayToDate(notification.report.createdDate)}</p>
-            </HoverDiv>
-          </Link>
-        ))
-      )}
-    </Container>
-  );
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <Container>
+            {notifications.length === 0 ? (
+                <Row className="d-flex justify-content-center align-items-center mt-5">
+                    <Col xs="auto" className="text-center">
+                        <h4>No notifications found</h4>
+                    </Col>
+                </Row>
+            ) : (
+                notifications.map((notification) => (
+                    <Link
+                        key={notification.id}
+                        to={`${notification.id}`}
+                        style={{textDecoration: "none"}}
+                    >
+                        <HoverDiv>
+                            <h4>{notification.report.title}</h4>
+                            <p>{notification.report.description}</p>
+                            <p>{arrayToDate(notification.report.createdDate)}</p>
+                        </HoverDiv>
+                    </Link>
+                ))
+            )}
+        </Container>
+    );
 };
 
 export default NotificationPage;
--- Front-end/src/user_settings/OrderHistory.jsx
+++ Front-end/src/user_settings/OrderHistory.jsx
@@ -1,99 +1,107 @@
-import { useEffect, useState } from "react";
-import { Badge, Card, Col, Container, Row } from "react-bootstrap";
-import { useAuth } from "../provider/AuthProvider";
-import { jwtDecode } from "jwt-decode";
+import React, {useEffect, useState} from "react";
+import {Badge, Card, Col, Container, Row} from "react-bootstrap";
+import {useAuth} from "../provider/AuthProvider";
+import {jwtDecode} from "jwt-decode";
 import axios from "axios";
 import ServerUrl from "../reusable/ServerUrl";
 import arrayToDate from "../reusable/ArrayToDate";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import Loader from "../reusable/Loader";
 import noImage from "../assets/no_image.jpg";
 
 function OrderHistory() {
-  const [data, setData] = useState([]);
-  const { token } = useAuth();
-  const [hover, setHover] = useState(false);
-  const [hoverId, setHoverId] = useState();
+    const [data, setData] = useState([]);
+    const {token} = useAuth();
+    const [hover, setHover] = useState(false);
+    const [hoverId, setHoverId] = useState();
 
-  useEffect(() => {
-    if (token) {
-      const decodedToken = jwtDecode(token);
-      axios(`${ServerUrl}/api/order/account/${decodedToken.id}`, {
-        method: "GET",
-        headers: { "Content-Type": "Application/json" },
-      }).then((res) => setData(res.data.responseList.orders));
+    useEffect(() => {
+        if (token) {
+            const decodedToken = jwtDecode(token);
+            axios(`${ServerUrl}/api/order/account/${decodedToken.id}`, {
+                method: "GET",
+                headers: {"Content-Type": "Application/json"},
+            }).then((res) => setData(res.data.responseList.orders));
+        }
+    }, [token]);
+
+    if (!data) {
+        return (
+            <Loader/>
+        );
     }
-  }, [token]);
 
-  if (!data) {
+    const getOrder = data.map((i) => {
+        return (
+            <Col className="p-4" md={4} key={i.id}>
+                <Link
+                    to="/user_setting_page/order_detail_page"
+                    state={i.id}
+                    className=" link-opacity-50-hover text-decoration-none"
+                >
+                    <Card
+                        onMouseEnter={() => {
+                            setHoverId(i.id);
+                            setHover(true);
+                        }}
+                        onMouseLeave={() => {
+                            setHoverId(null);
+                            setHover(false);
+                        }}
+                        className=" link-opacity-50-hover"
+                        style={{
+                            transition: " transform 300ms ease-out ",
+                            transform: hoverId === i.id ? "scale(1.02)" : "scale(1.00)",
+                            width: "20rem",
+                        }}
+                    >
+                        <Card.Img
+                            className="rounded-0"
+                            onError={(ev) => (ev.target.src = noImage)}
+                            variant="top"
+                            src={i.imageURL || noImage}
+                            alt="Order Image"
+                        />
+                        <Card.Body
+                            className="position-sticky"
+                            style={{
+                                height: "100%",
+                                transition: " height 100s linear ",
+                            }}
+                        >
+                            <Card.Text>{i.id}</Card.Text>
+                            <Card.Text>{arrayToDate(i.createdDate)}</Card.Text>
+                            <Badge
+                                bg={
+                                    i.status === "ORDER_COMPLETED"
+                                        ? "success"
+                                        : i.status === "CANCEL"
+                                            ? "danger"
+                                            : "warning"
+                                }
+                            >
+                                {i.status}
+                            </Badge>
+                        </Card.Body>
+                    </Card>
+                </Link>
+            </Col>
+        );
+    });
+
     return (
-      <Loader/>
+        <Container>
+            {data.length === 0 ? (
+                <Row className="d-flex justify-content-center align-items-center mt-5">
+                    <Col xs="auto" className="text-center">
+                        <h4>No orders found</h4>
+                    </Col>
+                </Row>
+            ) : (
+                <Row className="mt-4">{getOrder}</Row>
+            )}
+        </Container>
     );
-  }
-
-  const getOrder = data.map((i) => {
-    return (
-      <Col className="p-4" md={4} key={i.id}>
-        <Link
-          to="/user_setting_page/order_detail_page"
-          state={i.id}
-          className=" link-opacity-50-hover text-decoration-none"
-        >
-          <Card
-            onMouseEnter={() => {
-              setHoverId(i.id);
-              setHover(true);
-            }}
-            onMouseLeave={() => {
-              setHoverId(null);
-              setHover(false);
-            }}
-            className=" link-opacity-50-hover"
-            style={{
-              transition: " transform 300ms ease-out ",
-              transform: hoverId === i.id ? "scale(1.02)" : "scale(1.00)",
-              width: "20rem",
-            }}
-          >
-            <Card.Img
-              className="rounded-0"
-              onError={(ev) => (ev.target.src = noImage)}
-              variant="top"
-              src={i.imageURL || noImage}
-              alt="Order Image"
-            />
-            <Card.Body
-              className="position-sticky"
-              style={{
-                height: "100%",
-                transition: " height 100s linear ",
-              }}
-            >
-              <Card.Text>{i.id}</Card.Text>
-              <Card.Text>{arrayToDate(i.createdDate)}</Card.Text>
-              <Badge
-                bg={
-                  i.status === "ORDER_COMPLETED"
-                    ? "success"
-                    : i.status === "CANCEL"
-                    ? "danger"
-                    : "warning"
-                }
-              >
-                {i.status}
-              </Badge>
-            </Card.Body>
-          </Card>
-        </Link>
-      </Col>
-    );
-  });
-
-  return (
-    <Container>
-      <Row className="mt-4">{getOrder}</Row>
-    </Container>
-  );
 }
 
 export default OrderHistory;
