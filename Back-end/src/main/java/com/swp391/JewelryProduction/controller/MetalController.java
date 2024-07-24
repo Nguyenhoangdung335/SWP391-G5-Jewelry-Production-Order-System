31541c95174b4ebded48db850a6f6f4ce2da3853
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-07-25 02:18:22 +0700

    Create Metal Controller for Metal CRUD operations

---
+++ Back-end/src/main/java/com/swp391/JewelryProduction/controller/MetalController.java
@@ -0,0 +1,12 @@
+package com.swp391.JewelryProduction.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/metal")
+@RequiredArgsConstructor
+public class MetalController {
+
+}
