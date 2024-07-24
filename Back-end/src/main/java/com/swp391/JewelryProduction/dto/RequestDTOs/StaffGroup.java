da5b80d1d2eef16ee864de3ec927aa50526822d2
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-07-25 02:22:48 +0700

    Create Validation for for StaffGroup DTO attributes

--- Back-end/src/main/java/com/swp391/JewelryProduction/dto/RequestDTOs/StaffGroup.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/dto/RequestDTOs/StaffGroup.java
@@ -1,17 +1,29 @@
 package com.swp391.JewelryProduction.dto.RequestDTOs;
 
 import com.swp391.JewelryProduction.pojos.Staff;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class StaffGroup {
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "^ACC\\d{5}$")
     private String saleStaffID;
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "^ACC\\d{5}$")
     private String designStaffID;
+    @NotNull
+    @NotEmpty
+    @Pattern(regexp = "^ACC\\d{5}$")
     private String productionStaffID;
 }
