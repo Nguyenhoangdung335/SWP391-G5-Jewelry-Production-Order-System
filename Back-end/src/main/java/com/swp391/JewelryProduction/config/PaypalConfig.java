23a22e430f88f865a2e502a22924a08a612b1444
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-07-24 11:35:05 +0700

    Change to explicitly using OAuthTokenCredential
    Despite deprecated but provide more clarity

--- Back-end/src/main/java/com/swp391/JewelryProduction/config/PaypalConfig.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/config/PaypalConfig.java
@@ -1,9 +1,14 @@
 package com.swp391.JewelryProduction.config;
 
 import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.OAuthTokenCredential;
+import com.paypal.base.rest.PayPalRESTException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class PaypalConfig {
@@ -18,7 +23,26 @@ public class PaypalConfig {
     private String mode;
 
     @Bean
-    public APIContext apiContext() {
-        return new APIContext(clientID, clientSecret, mode);
+    public Map<String, String> paypalSdkConfig () {
+        Map<String, String> configMap = new HashMap<>();
+        configMap.put("mode", mode);
+        return configMap;
     }
+
+    @Bean
+    public OAuthTokenCredential oAuthTokenCredential () {
+        return new OAuthTokenCredential(clientID, clientSecret, paypalSdkConfig());
+    }
+
+    @Bean
+    public APIContext apiContext() throws PayPalRESTException {
+        APIContext context = new APIContext(oAuthTokenCredential().getAccessToken());
+        context.setConfigurationMap(paypalSdkConfig());
+        return context;
+    }
+
+//    @Bean
+//    public APIContext apiContext () {
+//        return new APIContext(clientID, clientSecret, mode);
+//    }
 }
