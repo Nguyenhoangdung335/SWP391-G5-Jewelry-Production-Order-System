0553f839796f90a2da72058a40d7377eec28e83c
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-07-25 02:17:52 +0700

    Add Gemstone Endpoints
    Move Gemstone Endpoints from AdminController
    Create search endpoints for finding list of qualified gemstones

--- Back-end/src/main/java/com/swp391/JewelryProduction/controller/GemstoneController.java
+++ Back-end/src/main/java/com/swp391/JewelryProduction/controller/GemstoneController.java
@@ -1,14 +1,24 @@
 package com.swp391.JewelryProduction.controller;
 
+import com.swp391.JewelryProduction.dto.RequestDTOs.GemstoneRequest;
+import com.swp391.JewelryProduction.enums.gemstone.GemstoneClarity;
+import com.swp391.JewelryProduction.enums.gemstone.GemstoneColor;
+import com.swp391.JewelryProduction.enums.gemstone.GemstoneCut;
+import com.swp391.JewelryProduction.enums.gemstone.GemstoneShape;
+import com.swp391.JewelryProduction.pojos.designPojos.Gemstone;
 import com.swp391.JewelryProduction.services.crawl.CrawlDataService;
 import com.swp391.JewelryProduction.services.gemstone.GemstoneService;
 import com.swp391.JewelryProduction.util.Response;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,6 +32,58 @@ public class GemstoneController {
         return Response.builder()
                 .responseList(gemstoneService.getGemstoneFactor())
                 .response("metal", crawlDataService.getAllMetalPrices())
+                .buildEntity();
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Response> getAllGemstones(
+            @RequestParam("page") int page,
+            @RequestParam("size") int pageSize,
+            @RequestParam(name = "sortBy", defaultValue = "caratWeightFrom") String sortBy
+    ) {
+        Page<Gemstone> gemstonePage = gemstoneService.findAll(page, pageSize, sortBy);
+        return Response.builder()
+                .status(HttpStatus.OK)
+                .message("Request sent successfully.")
+                .response("gemstone", gemstonePage.getContent())
+                .response("totalPages", gemstonePage.getTotalPages())
+                .response("totalElements", gemstonePage.getTotalElements())
+                .buildEntity();
+    }
+
+    @PostMapping("/search")
+    public ResponseEntity<Response> getSearchedGemstones (
+            @RequestBody @Valid GemstoneRequest gemstoneRequest
+    ) {
+        return Response.builder()
+                .response("gemstone", gemstoneService.searchByProperties(gemstoneRequest))
+                .buildEntity();
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Response> createGemstoneType(@RequestBody @Valid Gemstone gemstone) {
+        return Response.builder()
+                .status(HttpStatus.OK)
+                .message("Request sent successfully.")
+                .response("gemstone", gemstoneService.createGemstone(gemstone))
+                .buildEntity();
+    }
+
+    @PutMapping("")
+    public ResponseEntity<Response> updateGemstoneType(@RequestBody @Valid Gemstone gemstone) {
+        return Response.builder()
+                .status(HttpStatus.OK)
+                .message("Request sent successfully.")
+                .response("gemstone", gemstoneService.updateGemstone(gemstone))
+                .buildEntity();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Response> deleteGemstone(@PathVariable("id") long id) {
+        gemstoneService.deleteGemstone(id);
+        return Response.builder()
+                .status(HttpStatus.OK)
+                .message("Request sent successfully.")
                 .buildEntity();
     }
 }
