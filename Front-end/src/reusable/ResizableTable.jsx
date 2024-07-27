2dcb19d779c1d850181cea9fb8c6f1ff94edfcb0
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-07-27 17:28:04 +0700

    Create ResizableTable.jsx
    Fork ResizableTable from user @rachitpsolanki

---
+++ Front-end/src/reusable/ResizableTable.jsx
@@ -0,0 +1,49 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import ColumnResizer from "column-resizer";
+import { Table } from "react-bootstrap";
+
+const ResizableTable = ({ children, resizable, resizeOptions }) => {
+  const tableRef = useRef(null);
+  const [resize, setResize] = useState(null);
+
+  const enableResize = useCallback(() => {
+    if (tableRef.current && !resize) {
+      const resizeFn = new ColumnResizer(tableRef.current, {
+        ...resizeOptions,
+        liveDrag: true,
+        resizeMode: "flex",
+      });
+      // Remove className to avoid conflicts
+      tableRef.current.className = tableRef.current?.className?.replace(
+        "grip-padding",
+        ""
+      );
+      setResize(resizeFn);
+    } else if (resize) {
+      resize.reset(resizeOptions);
+    }
+  }, [resize, resizeOptions]);
+
+  const disableResize = useCallback(() => {
+    if (resize) {
+      resize.reset({ disable: true });
+    }
+  }, [resize]);
+
+  useEffect(() => {
+    if (tableRef.current && resizable) {
+      enableResize();
+    }
+    return () => {
+      disableResize();
+    };
+  }, [tableRef, resizable, enableResize, disableResize]);
+
+  return (
+    <Table striped hover bordered ref={tableRef}>
+      {children}
+    </Table>
+  );
+};
+
+export default ResizableTable;
