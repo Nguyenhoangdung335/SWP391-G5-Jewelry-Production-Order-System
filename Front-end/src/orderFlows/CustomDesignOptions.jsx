7dc25a7b059ca5901039a15bce24ea158834c2ba
Nguyenhoangdung335 <dungnhse180163@fpt.edu.vn>
2024-07-30 17:16:16 +0700

    Update CustomDesignOptions.jsx
    Update RenderSpecificationForm to use MetalForm

--- Front-end/src/orderFlows/CustomDesignOptions.jsx
+++ Front-end/src/orderFlows/CustomDesignOptions.jsx
@@ -3,41 +3,16 @@ import { useCallback, useEffect, useState } from "react";
 import { Button, Col, Container, Form, Modal, Row, Table } from "react-bootstrap";
 import ServerUrl from "../reusable/ServerUrl";
 import CreateRequest from "./CreateRequest";
-import { ChainType, DesignStyle, JewelryType, Length, Occasion, Texture, GemstoneForm, MetalWeight } from "./DesignOptionForms";
+import { ChainType, DesignStyle, JewelryType, Length, Occasion, Texture, GemstoneForm, MetalWeight, MetalForm } from "./DesignOptionForms";
 
 function OrderPage1() {
   const [showModal, setShowModal] = useState(false);
   const [productSpecId, setProductSpecId] = useState(null);
 
-  const handleSubmit = (e, formState, selectedGemstoneProp) => {
-    e.preventDefault();
-    const productSpecification = {
-      type: formState.selectedType,
-      style: formState.selectedStyle,
-      occasion: formState.selectedOccasion,
-      length: formState.selectedLength,
-      metal: formState.selectedMetal,
-      metalWeight: formState.selectedMetalWeight,
-      texture: formState.selectedTexture,
-      chainType: formState.selectedChainType,
-      gemstone: selectedGemstoneProp.selectedGemstone,
-      gemstoneWeight: selectedGemstoneProp.selectedGemstoneWeight
-    };
-
-    axios({
-      method: "POST",
-      url: `${ServerUrl}/api/products/customize`,
-      headers: { "Content-Type": "application/json" },
-      data: productSpecification,
-    })
-      .then((response) => {
-        setProductSpecId(response.data.responseList.productSpecification.id);
-        setShowModal(true);
-      })
-      .catch((error) => {
-        console.log("There is an error in this code" + error);
-      });
-  };
+  const handleMakeRequest = (productSpecId) => {
+    setShowModal(true);
+    setProductSpecId(productSpecId);
+  }
 
   const handleRequestCanceled = (e) => {
     setShowModal(false);
@@ -58,7 +33,7 @@ function OrderPage1() {
       <h3 className="fw-bold" style={{ margin: "30px 0px 30px" }}>
         Create Your Dream Jewelry.
       </h3>
-      <RenderSpecificationForm handleSubmit={handleSubmit} />
+      <RenderSpecificationForm handleMakeRequest={handleMakeRequest} />
       <Modal show={showModal} size="lg" backdrop="static" keyboard={false}>
         <Modal.Header>
           <Modal.Title>Create Request</Modal.Title>
@@ -71,10 +46,7 @@ function OrderPage1() {
   );
 }
 
-const RenderSpecificationForm = ({handleSubmit, initialSpecs = null}) => {
-  const [metalData, setMetalData] = useState([]);
-  const [metalName, setMetalName] = useState([]);
-  const [metalUnit, setMetalUnit] = useState({});
+const RenderSpecificationForm = ({handleSubmit, initialSpecs = null, handleMakeRequest}) => {
   const [gemstoneData, setGemstoneData] = useState({
     names: [],
     shapes: [],
@@ -91,22 +63,23 @@ const RenderSpecificationForm = ({handleSubmit, initialSpecs = null}) => {
     selectedLength: initialSpecs? initialSpecs.length :"",
     selectedTexture: initialSpecs? initialSpecs.texture :"",
     selectedChainType: initialSpecs? initialSpecs.chainType :"",
-    selectedMetalName: initialSpecs? initialSpecs.metal.name :"",
-    selectedMetalUnit: initialSpecs? initialSpecs.metal.unit :"",
-    selectedMetal: initialSpecs? initialSpecs.metal :null,
-    selectedMetalWeight: initialSpecs? initialSpecs.metalWeight :"",
     selectedGemstone: initialSpecs? initialSpecs.gemstone :null,
     selectedGemstoneWeight: initialSpecs? initialSpecs.gemstoneWeight :"",
-    metalUnits: initialSpecs?  metalUnit[initialSpecs.metal.name] || [] :null,
   });
   const [selectedGemstoneProp, setSelectedGemstoneProp] = useState({
-    selectedGemstoneName: initialSpecs.gemstone? initialSpecs.gemstone.name: "",
-    selectedGemstoneShape: initialSpecs.gemstone? initialSpecs.gemstone.shape: "",
-    selectedGemstoneCut: initialSpecs.gemstone? initialSpecs.gemstone.cut: "",
-    selectedGemstoneClarity: initialSpecs.gemstone? initialSpecs.gemstone.clarity: "",
-    selectedGemstoneColor: initialSpecs.gemstone? initialSpecs.gemstone.color: "",
+    selectedGemstoneName: initialSpecs?.gemstone? initialSpecs.gemstone.name: "",
+    selectedGemstoneShape: initialSpecs?.gemstone? initialSpecs.gemstone.shape: "",
+    selectedGemstoneCut: initialSpecs?.gemstone? initialSpecs.gemstone.cut: "",
+    selectedGemstoneClarity: initialSpecs?.gemstone? initialSpecs.gemstone.clarity: "",
+    selectedGemstoneColor: initialSpecs?.gemstone? initialSpecs.gemstone.color: "",
     selectedGemstoneWeight: initialSpecs? initialSpecs.gemstoneWeight: "",
     selectedGemstone: initialSpecs? initialSpecs.gemstone: null,
+  });
+  const [selectedMetalProp, setSelectedMetalProp] = useState({
+    selectedMetalName: initialSpecs? initialSpecs.metal.name: "",
+    selectedMetalUnit: initialSpecs? initialSpecs.metal.unit: "",
+    selectedMetal: initialSpecs? initialSpecs.metal: null,
+    selectedMetalWeight: initialSpecs? initialSpecs.metalWeight: "",
   });
 
   useEffect(() => {
@@ -115,7 +88,6 @@ const RenderSpecificationForm = ({handleSubmit, initialSpecs = null}) => {
         const response = await axios.get(`${ServerUrl}/api/gemstone/factors`);
         if (response.status === 200) {
           const data = response.data.responseList;
-          setMetalData(data.metal);
           setGemstoneData({
             names: data.names,
             shapes: data.shapes,
@@ -172,65 +144,48 @@ const RenderSpecificationForm = ({handleSubmit, initialSpecs = null}) => {
   }, [formState.selectedType]);
 
   useEffect(() => {
-    if (metalData.length > 0) {
-      const uniqueMetalNames = [...new Set(metalData.map((item) => item.name))];
-      setMetalName(uniqueMetalNames);
-
-      const unitsMap = {};
-      uniqueMetalNames.forEach((name) => {
-        unitsMap[name] = metalData
-          .filter((metal) => metal.name === name)
-          .map((metal) => metal.unit);
-      });
-      setMetalUnit(unitsMap);
-    }
-  }, [metalData]);
-
-  useEffect(() => {
-    if (formState.selectedMetalName) {
-      const units = metalUnit[formState.selectedMetalName] || [];
+    if (selectedMetalProp.selectedMetalName) {
       setFormState((prev) => ({
         ...prev,
-        selectedMetalUnit: formState.selectedMetalUnit? formState.selectedMetalUnit: units[0],
-        selectedMetal: metalData.find(
-          (metal) =>
-            metal.name === formState.selectedMetalName && metal.unit === units[0]
-        ),
         selectedChainType: ["Necklace", "Bracelet", "Anklet"].includes(formState.selectedType) ? "Default": "NaN",
         selectedTexture: "Default",
-        selectedMetalWeight: 1,
-        metalUnits: units,
       }));
     }
-  }, [formState.selectedMetalName, metalData, metalUnit]);
+  }, [selectedMetalProp.selectedMetalName, formState.selectedType]);
+
+  const handleSubmitData = (e) => {
+    e.preventDefault();
+    const productSpecification = {
+      type: formState.selectedType,
+      style: formState.selectedStyle,
+      occasion: formState.selectedOccasion,
+      length: formState.selectedLength,
+      metal: selectedMetalProp.selectedMetal,
+      metalWeight: selectedMetalProp.selectedMetalWeight,
+      texture: formState.selectedTexture,
+      chainType: formState.selectedChainType,
+      gemstone: selectedGemstoneProp.selectedGemstone,
+      gemstoneWeight: selectedGemstoneProp.selectedGemstoneWeight
+    };
+
+    axios({
+      method: "POST",
+      url: `${ServerUrl}/api/products/customize`,
+      headers: { "Content-Type": "application/json" },
+      data: productSpecification,
+    })
+    .then((response) => {
+      if (handleMakeRequest)
+        handleMakeRequest(response.data.responseList.productSpecification.id);
+    })
+    .catch((error) => {
+      console.log("There is an error in this code" + error);
+    });
+  };
 
   const handleChange = (e) => {
     const { name, value } = e.target;
     setFormState((prev) => ({ ...prev, [name]: value }));
-  };
-
-  const handleChangeMetalUnit = (e) => {
-    const chosenUnit = e.target.value;
-    setFormState((prev) => ({
-      ...prev,
-      selectedMetalUnit: chosenUnit,
-    }));
-
-    if (formState.selectedMetalName) {
-      const chosenMetal = metalData.find(
-        (item) => {
-          console.log(item);
-          console.log(item.name === formState.selectedMetalName &&
-            item.unit === formState.selectedMetalUnit);
-          return item.name === formState.selectedMetalName &&
-          item.unit === formState.selectedMetalUnit
-      });
-      console.log(chosenMetal);
-      setFormState((prev) => ({
-        ...prev,
-        selectedMetal: chosenMetal,
-      }));
-    }
   };
 
   const handleChangeGemstone = useCallback((e) => {
@@ -240,14 +195,14 @@ const RenderSpecificationForm = ({handleSubmit, initialSpecs = null}) => {
     } else {
       setSelectedGemstoneProp((prev) => ({ ...prev, [name]: value }));
     }
-    if (name === "selectedGemstoneName" && value === "") {
+    if (name === "selectedGemstoneName" && value === "" && !value) {
       setSelectedGemstoneProp({
         selectedGemstoneName: null,
         selectedGemstoneShape: "",
         selectedGemstoneCut: "",
         selectedGemstoneClarity: "",
         selectedGemstoneColor: "",
-        selectedGemstoneWeight: "0.05",
+        selectedGemstoneWeight: "",
         selectedGemstone: null,
       });
     } else if (name === "selectedGemstoneName" && value) {
@@ -266,16 +221,18 @@ const RenderSpecificationForm = ({handleSubmit, initialSpecs = null}) => {
         selectedGemstone: null,
       }))
     }
-  }, [gemstoneData]);
+  }, []);
+
+  const handleChangeMetal = useCallback((e) => {
+    const { name, value } = e.target;
+    setSelectedMetalProp((prev) => ({...prev, [name]: value}));
+  }, []);
 
   const handleDisable = () => {
     const {
       selectedStyle,
       selectedOccasion,
       selectedType,
-      selectedMetalName,
-      selectedMetalUnit,
-      selectedMetalWeight,
       selectedLength
     } = formState;
 
@@ -284,22 +241,27 @@ const RenderSpecificationForm = ({handleSubmit, initialSpecs = null}) => {
       selectedGemstone
     } = selectedGemstoneProp;
 
+    const {
+      selectedMetal,
+      selectedMetalWeight,
+    } = selectedMetalProp;
+
     return !(
       selectedStyle &&
       selectedOccasion &&
       selectedType &&
-      selectedMetalName &&
-      selectedMetalUnit &&
+      selectedMetal &&
       selectedMetalWeight &&
       selectedMetalWeight > 0 &&
       selectedLength !== "0" &&
       selectedLength &&
       (!selectedGemstoneName || (selectedGemstoneName && selectedGemstone))
+      // (selectedMetalName && selectedMetal)
     );
   };
 
   return (
-    <Form onSubmit={(event) => handleSubmit(event, formState, selectedGemstoneProp)} className="mb-5">
+    <Form onSubmit={handleSubmitData} className="mb-5">
       <Container>
         <Row>
           <Col sm={12} md={6} lg={4}>
@@ -322,57 +284,19 @@ const RenderSpecificationForm = ({handleSubmit, initialSpecs = null}) => {
       {/* Metal */}
       {formState.selectedType && (
         <>
-          {/* Metal Name */}
-          <h5 className="pt-1">Material</h5>
-          <Form.Group className="mb-3">
-            <Form.Label>Metal*</Form.Label>
-            <Form.Select
-              name="selectedMetalName"
-              value={formState.selectedMetalName}
-              onChange={handleChange}
-              size="sm"
-            >
-              <option value="" disabled>
-                Choose one
-              </option>
-              {metalName.map((metal, index) => (
-                <option key={index} value={metal}>
-                  {metal}
-                </option>
-              ))}
-            </Form.Select>
-          </Form.Group>
+          <MetalForm onChange={handleChangeMetal} selectedType={formState.selectedType} selectedMetalData={selectedMetalProp}/>
 
-          {formState.selectedMetalName && (
-            <>
-              {/* Metal Units */}
-              <Form.Group className="mb-3">
-                <Form.Label>Metal Unit</Form.Label>
-                <Form.Select
-                  name="selectedMetalUnit"
-                  value={formState.selectedMetalUnit}
-                  onChange={handleChangeMetalUnit}
-                  size="sm"
-                >
-                  {formState.metalUnits &&
-                    formState.metalUnits.map((unit, index) => (
-                      <option key={index} value={unit}>
-                        {unit}
-                      </option>
-                    ))}
-                </Form.Select>
-              </Form.Group>
-
-              <MetalWeight onChange={handleChange} selectedUnit={formState.selectedMetalUnit} value={formState.selectedMetalWeight} />
-
-              {/* Metal Texture */}
-              <Texture value={formState.selectedTexture} onChange={handleChange} />
-
-              {/* Chain Type */}
+          {selectedMetalProp.selectedMetalName && (
+            <Row>
+              <Col sm={12} md={true}>
+                <Texture onChange={handleChange} value={formState.selectedTexture} />
+              </Col>
               {!["Rings", "Earrings"].includes(formState.selectedType) && (
-                <ChainType onChange={handleChange} value={formState.selectedChainType} />
+                  <Col sm={12} md={6}>
+                    <ChainType onChange={handleChange} value={formState.selectedChainType} />
+                  </Col>
               )}
-            </>
+            </Row>
           )}
         </>
       )}
@@ -390,33 +314,6 @@ const RenderSpecificationForm = ({handleSubmit, initialSpecs = null}) => {
       </div>
     </Form>
   )
-}
-
-const renderMetalTable = (selectedMetalName, metalData) => {
-  const metalList = metalData.find((m) => m.metal === selectedMetalName);
-
-  return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Metal</th>
-          <th>Unit</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        {metalList && metalList.length > 0 && (
-          metalList.map((metal, index) => (
-            <tr key={index}>
-              <td>{metal.metal}</td>
-              <td>{metal.unit}</td>
-              <td>{metal.price}</td>
-            </tr>
-          ))
-        )}
-      </tbody>
-    </Table>
-  );
 }
 
 export default OrderPage1;
